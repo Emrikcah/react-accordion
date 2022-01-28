67302b44274210e0d09e1eece82f216d4c9bf7a8
Emrikcah <curryandr3@gmail.com>
2022-01-28 05:33:54 -0500

    setup structure for app.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
+import { Questions } from "./components";
+import data from './data/data';
 
-import './App.css';
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      hello
-    </div>
-  );
+   return (
+      <main>
+         <div className="container">
+            <h3>questions and answers about login</h3>
+
+            <Questions data={data} />
+         </div>
+      </main>
+   );
 }
 
 export default App;
+
+//import data and pass it to Questions component
