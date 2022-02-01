b27eeea0ac79b9998f8148e927ee4c1a7029cb51
Emrikcah <curryandr3@gmail.com>
2022-02-01 12:31:17 -0500

    changed logic in toggle, removed comments

--- src/components/questions/Questions.js
+++ src/components/questions/Questions.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import {AiOutlineMinus,AiOutlinePlus} from 'react-icons/ai';
+import { AiOutlineMinus, AiOutlinePlus } from "react-icons/ai";
 import "./questions.css";
 
 const Questions = ({ data }) => {
@@ -7,29 +7,28 @@ const Questions = ({ data }) => {
    const [isShown, setIsShown] = useState(false);
    const [shownId, setShownId] = useState();
 
-   
    const toggle = (id) => {
-      setIsShown(isShown => !isShown)
-      if (!isShown) {
-         setShownId(id);
-      }else{
-         setShownId(0);
-      }
-      
-   }
+      setIsShown((isShown) => !isShown);
+      !isShown ? setShownId(id) : setShownId(0);
+      // if (!isShown) {
+      //    setShownId(id);
+      // }else{
+      //    setShownId(0);
+      // }
+   };
 
    return (
       <>
          {questions.map((question) => {
             const { id, title, info } = question;
-            
+
             return (
                <article key={id} className="question">
                   <header className="question_header">
                      <h4>{title}</h4>
-                  <button className="question_btn" onClick={()=> toggle(id)}>
-                     {isShown ? <AiOutlineMinus/> : <AiOutlinePlus/>}
-                  </button>
+                     <button className="question_btn" onClick={() => toggle(id)}>
+                        {isShown ? <AiOutlineMinus /> : <AiOutlinePlus />}
+                     </button>
                   </header>
                   {shownId === id && <p className="question_p">{info}</p>}
                </article>
@@ -41,12 +40,6 @@ const Questions = ({ data }) => {
 
 export default Questions;
 
-//setup html structure
-//loop over the data and display
-//create button to toggle state
-//pass the id to function to toggle that objects text
-//show/hide data when button is clicked
 
-//todo: how to show hide text based on id and isShown
 
 
